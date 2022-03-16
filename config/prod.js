4f8b8b73a69084aa9fdd9d7d821f9ef070b17d32
T Jang <taehunjang@ts-macbook-pro.attlocal.net>
2022-03-16 18:38:31 -0500

    finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+    GOOGLE_CLIENT_ID: process.env.GOOGLECLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLECLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
+};
