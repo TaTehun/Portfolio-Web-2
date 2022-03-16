6341a5664cb2106fe17d41707f4ef721f267298a
T Jang <taehunjang@ts-macbook-pro.attlocal.net>
2022-03-16 18:53:28 -0500

    resync

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === 'production') {
     // in production - return the prod set of keys
-    module.exports = require('./prod');
+    module.exports = require ('./prod');
 } else {
     // in developement = return the dev keys
     module.exports = require ('./dev');
