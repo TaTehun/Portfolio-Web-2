e5a58d62b2b8c0f7075ccd0dab2cb8c1bb16845d
T Jang <taehunjang@ts-macbook-pro.attlocal.net>
2022-03-27 05:05:54 -0500

    added billing and client side app

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,10 @@
+const { stripePublishableKey } = require("./dev");
+
 module.exports = {
     GOOGLE_CLIENT_ID: process.env.GOOGLECLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLECLIENT_SECRET,
     mongoURI: process.env.MONGO_URI,
-    cookieKey: process.env.COOKIE_KEY
+    cookieKey: process.env.COOKIE_KEY,
+    stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+    stripeSecretKey: process.env.STRIPE_SECRET_KEY
 };
